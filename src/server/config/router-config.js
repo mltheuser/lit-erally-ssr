39e057d7ca63bd164a10d878abd8dddf572512ea
Malte Heuser <malte.heuser@ing.com>
2024-04-20 10:26:26 +0200

    improved server setup

--- src/server/config/router-config.js
+++ src/server/config/router-config.js
@@ -1,40 +1,8 @@
 import Router from '@koa/router';
-
-import html from '../../webcomponent-lib/crossPlatform/html-parser.js';
-import hydrate from '../../webcomponent-lib/server/ss-hydrate.js';
-import render from '../../webcomponent-lib/server/ssr.js';
+import homeRoutes from '../routes/home.js'
 
 const router = new Router();
 
-router.get('/', async (ctx) => {
-    const htmlString = html`
-        <p>text</p>
-        <div>
-            <simple-button text=${[1, 2, 3]}>
-                <span>Let's have some different text!</span>
-                <counter-list counts="[129, 130]"></counter-list>
-            </simple-button>
-        </div>
-    `
-    const htmlRender = render(hydrate(htmlString));
-
-    console.log(htmlRender)
-
-    ctx.type = 'text/html';
-    ctx.body = htmlRender;
-});
-
-router.get('/buttonClicked', async (ctx) => {
-    const htmlString = html`
-        <p>Thanks for clicking</p>
-        <p>I appreciate that</p>
-    `
-    const htmlRender = render(hydrate(htmlString));
-
-    console.log(htmlRender)
-
-    ctx.type = 'text/html';
-    ctx.body = htmlRender;
-});
+router.use(homeRoutes.routes(), homeRoutes.allowedMethods());
 
 export default router;
